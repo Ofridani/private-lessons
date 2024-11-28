7955d446282c10cdbb779e130c0f099fd36d372b
Ofridani <ofri.daniel100@gmail.com>
2024-11-28 20:44:52 +0200

    Raz2

---
+++ RazShahmoon2/RazShahmoon2/Program.cs
@@ -0,0 +1,19 @@
+﻿int x = int.Parse(Console.ReadLine()); // 16
+bool hasSeven = false;
+while (x > 0 && !hasSeven)
+{
+    if (x % 10 == 7)
+    {
+        hasSeven = true;
+    }
+    x /= 10;
+}
+
+if (hasSeven)
+{
+    Console.WriteLine("This number contains the digit 7!");
+}
+else
+{
+    Console.WriteLine("This number does not contain the digit 7!");
+}
---
+++ RazShahmoon3/RazShahmoon3/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Please enter a 5-digit number:");
+int x = int.Parse(Console.ReadLine());
+int newNum = 0;
+
+for (int i = 0; i < 5; i++)
+{
+    if (x % 10 == 9)
+    {
+        newNum += 0;
+    }
+    else
+    {
+        newNum += ((x % 10) + 1) * (int)Math.Pow(10, i);
+    }
+    x /= 10;
+}
+Console.WriteLine();
