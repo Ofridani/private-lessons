54a3e27dfed1142746bac7e4ea72ed3f7f5ebabd
Ofridani <ofri.daniel100@gmail.com>
2024-12-01 13:31:37 +0200

    Testday

---
+++ ArielZaibert2/ArielZaibert2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Data;
+
+int num = int.Parse(Console.ReadLine());
+string str = "";
+int sum = 0;
+for (int i = 1; i <= num; i++)
+{
+    if (i == num)
+    {
+        str += i + "=";
+    }
+    else
+    {
+        str += i + "+";
+    }
+    sum += i;
+}
+
+Console.WriteLine(str + sum);
+
---
+++ ArielZaibert3/ArielZaibert3/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Security.Cryptography;
+using System.Transactions;
+
+//Console.WriteLine("Enter 10 numbers");
+//int max = int.Parse(Console.ReadLine());
+//for (int i=1; i<10; i++)
+//{
+//    int num = int.Parse(Console.ReadLine());
+//    if (num>max)
+//    {
+//        max = num;
+//    }
+//}
+//Console.WriteLine($"The maximum number is {max}");
+
+string lorem = "Lorem ipsu m dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
+int count = 0;
+char c = Console.ReadLine()[0];
+for (int i=0; i < lorem.Length; i++)
+{
+    if (lorem[i]==c)
+    {
+        count++;
+    }
+}
+Console.WriteLine(count);
