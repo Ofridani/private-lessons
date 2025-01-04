e5f92fabc479078b201583d165c9f65e14837eac
Ofridani <ofri.daniel100@gmail.com>
2025-01-04 17:35:53 +0200

    Lowercase

---
+++ AZ/ArielZaibert4/ArielZaibert4/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace ArielZaibert4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            printCheckerBoard();
+        }
+
+        static void printStars()
+        {
+            for (int i = 1; i <= 5; i++)
+            {
+                for (int j = 0; j < i; j++) // prints *, i times
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine(); // goes down a line
+            }
+        }
+
+        static void printCheckerBoard()
+        {
+            for (int row = 1; row < 5; row++)
+            {
+                if (row %2==0)
+                {
+                    Console.Write(" ");
+                }
+                for (int j = 0; j < 5; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ AZ/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Before:");
+            Console.WriteLine("Hello^ World/");
+            Console.WriteLine();
+            Console.WriteLine("After:");
+            Console.WriteLine(LowerString("Hello^ World/"));
+        }
+
+        static char LowerChar(char x)
+        {
+            if (x<'A' || x>'Z')
+            {
+                return x;
+            }
+
+            int num = (int)x;
+            num += 32;
+            return (char)num;
+        }
+
+        static string LowerString(string y)
+        {
+            string newy = "";
+            for (int i=0; i<y.Length; i++)
+            {
+                newy+=LowerChar(y[i]);
+            }
+            return newy;
+        }
+    }
+}
