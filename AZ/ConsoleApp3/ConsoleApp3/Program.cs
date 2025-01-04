bd23434f0e1eab338b32bf557a73ebb8b6d424c5
Ofridani <ofri.daniel100@gmail.com>
2025-01-04 19:00:31 +0200

    CountEvenDigits

---
+++ AZ/ConsoleApp3/ConsoleApp3/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace ConsoleApp3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i=1; i<=100; i++)
+            {
+                Console.WriteLine($"{i} {CountEvenDigits(i)}");
+            }
+        }
+        static int CountEvenDigits(int num)
+        {
+            int count = 0;
+            while (num>0)
+            {
+                if ((num%10)%2==0)
+                {
+                    count += 1;
+                }    
+                num /= 10;
+            }
+            return count;
+        }
+    }
+}
