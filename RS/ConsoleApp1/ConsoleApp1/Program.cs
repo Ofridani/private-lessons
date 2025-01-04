97c5753a63585d75f60ed3331e31bdbd17a4a90e
Ofridani <ofri.daniel100@gmail.com>
2025-01-04 19:48:21 +0200

    RazChar

---
+++ RS/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            char c = LowerChar('z');
+            Console.WriteLine(c);
+        }
+        static char LowerChar(char myChar)
+        {
+            if (myChar >= 'A' && myChar <= 'Z') // is myChar uppercase?
+            {
+                return (char)(myChar - ('A' - 'a')); // return myChar as lowercase.
+            }
+            return myChar; // if not return myChar as usual.
+        }
+
+        static string LowerString(string str)
+        {
+            string newStr = "";
+            for (int i = 0; i < str.Length; i++)
+            {
+                newStr += LowerChar(str[i]);
+            }
+            return newStr;
+
+        }
+
+    }
+}
