050b8fdf35b3f9ad073ca0f5e24e2b9709e792e4
Ofridani <ofri.daniel100@gmail.com>
2025-01-12 20:37:12 +0200

    OriQueue

---
+++ OriWittlin/ConsoleApp3/ConsoleApp3/Program.cs
@@ -0,0 +1,108 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ConsoleApp3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<string> q = new Queue<string>();
+            string[] balls = { "white", "black", "gold", "brown" };
+            foreach (string ball in balls)
+            {
+                q.Insert(ball);
+            }
+            Console.WriteLine();
+        }
+
+
+        static int QueueLength(Queue<int> q)
+        {
+            int len = 0;
+            Queue<int> temp = new Queue<int>();
+            while (!q.IsEmpty())
+            {
+                temp.Insert(q.Remove());
+                len++;
+            }
+            while (!temp.IsEmpty())
+            {
+                q.Insert(temp.Remove());
+            }
+            return len;
+        }
+
+        static Queue<T> CopyQueue<T>(Queue<T> Q)
+        {
+            Queue<T> copyQ = new Queue<T>();
+            Queue<T> tempQ = new Queue<T>();
+            T n;
+            while (!Q.IsEmpty())
+            {
+                n = Q.Remove();
+                copyQ.Insert(n);
+                tempQ.Insert(n);
+            }
+            while (!tempQ.IsEmpty())
+            {
+                Q.Insert(tempQ.Remove());
+            }
+            return copyQ;
+        }
+
+        public static bool GuessGold(Queue<string> q, int guess)
+        {
+            Queue<string> copy = CopyQueue(q);
+            for (int i = 0; i < guess; i++)
+            {
+                copy.Insert(copy.Remove());
+            }
+            if (copy.Head() == "gold")
+            {
+                return true;
+            }
+            return false;
+        }
+        public static int GetMin(Queue<int> q)
+        {
+            int min = 0;
+            Queue<int> temp = CopyQueue(q);
+            while (!temp.IsEmpty())
+            {
+                if (temp.Head() > min)
+                {
+                    min = temp.Remove();
+                }
+            }
+            return min;
+        }
+        public static Queue<int> Merge(Queue<int> q1, Queue<int> q2)
+        {
+            Queue<int> MergedQ = new Queue<int>();
+            while (!q1.IsEmpty() || !q2.IsEmpty())
+            {
+                if (q1.Head() < q2.Head())
+                {
+                    MergedQ.Insert(q1.Remove());
+                }
+                MergedQ.Insert(q2.Remove());
+            }
+            Queue<int> NotEmptyQ;
+
+            if (q1.IsEmpty())
+            {
+                NotEmptyQ = q2;
+            }
+            else
+            {
+                NotEmptyQ = q1;
+            }
+
+            while (!NotEmptyQ.IsEmpty())
+            {
+                MergedQ.Insert(NotEmptyQ.Remove());
+            }
+            return MergedQ;
+        }
+    }
+}
---
+++ RS/ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,62 @@
+﻿namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(CalcTicketPrice(30, true));  
+        }
+
+        static double CalcTicketPrice(double km, bool hasPass)
+        {
+            double price = 0;
+            if (km <= 5)
+            {
+                price += 5;
+            }
+            else if (km > 5 && km <= 20)
+            {
+                price += 5;
+                price += (km - 5) * 2;
+                
+            }
+            else
+            {
+                price += 5;
+                price += 20 * 2;
+                price += (km - 5 - 20) * 3;
+            }
+            if (hasPass)
+            {
+                price *= 0.8;
+            }
+            return price;
+        }
+
+        static bool IsDigitsAscending(int n)
+        {
+            while (n > 0)
+            {
+                if ((n / 10) % 10 >= n % 10)
+                {
+                    return false;
+                }
+                n /= 10;
+            }
+            return true;
+        }
+
+        static bool HasEvenDigit(int num)
+        {
+            while (num > 0)
+            {
+                if ((num % 10) % 2 == 0)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+    }
+}
